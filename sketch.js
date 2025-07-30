6401d508812a22b06f1e34742fd51d935f93b8dc
luizasilva22 <141179628+luizasilva22@users.noreply.github.com>
2025-07-30 13:41:11 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function draw() {
   background(190, 290, 390); //cor de fundo
   rotateY(millis()/1000); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(200, 300); //esfera 3D
+  sphere(600, 900); //esfera 3D
   
   
 }
